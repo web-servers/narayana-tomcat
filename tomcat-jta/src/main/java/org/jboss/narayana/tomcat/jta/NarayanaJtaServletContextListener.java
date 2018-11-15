7a8ef495f64bc44ce9739be160bddf3b749c9746
Radovan Synek <rsynek@redhat.com>
2018-11-15 12:32:48 +0100

    JWS-1151 Initialize RecoveryManager when servlet context is created

--- tomcat-jta/src/main/java/org/jboss/narayana/tomcat/jta/NarayanaJtaServletContextListener.java
+++ tomcat-jta/src/main/java/org/jboss/narayana/tomcat/jta/NarayanaJtaServletContextListener.java
@@ -94,7 +94,7 @@ public class NarayanaJtaServletContextListener implements ServletContextListener
         initRecoveryModules();
         initOrphanFilters();
         initExpiryScanners();
-        RecoveryManager.manager();
+        RecoveryManager.manager().initialize();
         TxControl.enable();
         TransactionReaper.instantiate();
     }
--- tomcat-jta/src/test/java/org/jboss/narayana/tomcat/jta/integration/AbstractUnitCase.java
+++ tomcat-jta/src/test/java/org/jboss/narayana/tomcat/jta/integration/AbstractUnitCase.java
@@ -4,28 +4,17 @@ import org.jboss.narayana.tomcat.jta.NarayanaJtaServletContextListener;
 import org.junit.After;
 import org.junit.Before;
 
-/**
- * Please note that running multiple test cases without the {@link TestSuite} will result in {@link IllegalStateException}
- * thrown by {@link com.arjuna.ats.arjuna.recovery.RecoveryManager#terminate},
- * as the {@link com.arjuna.ats.arjuna.recovery.RecoveryManager} cannot be instantiated and destroyed multiple times
- * in the same JVM.
- */
 public class AbstractUnitCase {
 
-    private NarayanaJtaServletContextListener listener;
+    private NarayanaJtaServletContextListener listener = new NarayanaJtaServletContextListener();
 
     @Before
     public void setUp() {
-        if (!TestSuite.isRunningInTestSuite()) {
-            listener = new NarayanaJtaServletContextListener();
-            listener.contextInitialized(null);
-        }
+        listener.contextInitialized(null);
     }
 
     @After
     public void tearDown() {
-        if (!TestSuite.isRunningInTestSuite()) {
-            listener.contextDestroyed(null);
-        }
+        listener.contextDestroyed(null);
     }
 }
